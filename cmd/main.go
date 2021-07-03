ef0be2b5dfb8fef081b68ef5227020dcc9d073d5
Nguyen Duc Hanh <duchanhstyle@gmail.com>
2021-07-03 17:44:35 +0700

    clean architecture and inject dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/fungerouscode/todo-app"
 	"github.com/fungerouscode/todo-app/pkg/handler"
+	"github.com/fungerouscode/todo-app/pkg/repository"
+	"github.com/fungerouscode/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
